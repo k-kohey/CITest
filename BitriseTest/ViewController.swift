341cf483d7b9c94186fa2e060dc271efbe29b2c9
k-kohey <kawagutsuchi@gmail.com>
2018-12-11 06:03:39 +0900

    first commit

---
+++ BitriseTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BitriseTest
+//
+//  Created by kawaguchi kohei on 2018/12/11.
+//  Copyright © 2018年 kawaguchi kohei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
